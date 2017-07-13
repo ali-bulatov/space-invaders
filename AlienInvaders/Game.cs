defe5f1ba13ee61ec4c5271cff22eb74e556434c
Nelson Shaw <shawne@sheridancollege.ca>
2017-07-13 18:50:27 -0400

    Added the Alien, ArcadeMachine, Bullet, EnemyBullet, Game, MotherShip, Player, and Shield classes and the Color, Direction, and Difficulty enumerations to the program. Added significant field variables and methods to the Player and Game class.

---
+++ AlienInvaders/Game.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienInvaders
+{
+    public enum GameDifficulty
+    {
+        Beginner = 1,
+        Easy,
+        Normal,
+        Hard,
+        Intense
+    }
+    /// <summary>
+    /// Represents a current game that is being played on the GamePage
+    /// </summary>
+    public class Game
+    {
+        private int _gameScore;
+
+        private Random _randomizer;
+
+        private byte _round;
+
+        private int _time;
+
+        private Player _player;
+
+        private List<EnemyBullet> _bulletList;
+
+        private List<Alien> _alienList;
+
+        private MotherShip _motherShip;
+
+        private List<Shield> _shieldList;
+
+        private GameDifficulty _difficulty;
+
+        public Game(GameDifficulty difficulty)
+        {
+
+        }
+
+        public void Play()
+        {
+
+        }
+
+        public void End()
+        {
+
+        }
+
+        public void UpdateScore()
+        {
+
+        }
+
+        public void ResetRound()
+        {
+
+        }
+
+        public void IncreaseSpeed()
+        {
+
+        }
+
+        public void DespawnAliens()
+        {
+
+        }
+
+        public void ShiftAliens()
+        {
+
+        }
+    }
+}
---
+++ AlienInvaders/Player.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienInvaders
+{
+    public enum Color
+    {
+        Red = 1,
+        Yellow,
+        Green,
+        Blue
+    }
+
+    public enum Direction
+    {
+        left,
+        right
+    }
+
+    public class Player
+    {
+        private double _position;
+
+        private bool _alive;
+
+        private byte _lives;
+
+        private double _speed;
+
+        private Bullet _bullet;
+
+        private Color _color;
+
+        private Direction _direction;
+
+        public Player(Byte lives, Color color)
+        {
+
+        }
+
+        public void Move()
+        {
+
+        }
+
+        public void OnShoot()
+        {
+
+        }
+
+        public void OnDeath()
+        {
+
+        }
+
+        public void Respawn()
+        {
+
+        }
+    }
+}
