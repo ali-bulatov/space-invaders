ca87f41c9d41259b9110484516118c4306087546
Jonathan Nellas <jonathanreinellas@hotmail.com>
2017-08-11 13:03:34 -0400

    Completed EnemyBullet class

--- AlienInvaders/EnemyBullet.cs
+++ AlienInvaders/EnemyBullet.cs
@@ -3,11 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace AlienInvaders
 {
     class EnemyBullet : Bullet
     {
+
+        private Image enemyBullet;
+
+        public bool DrawBullet()
+        {
+            if (IsAlive == false)
+            {
+                enemyBullet.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                Canvas.SetLeft(enemyBullet, xPosition);
+                xPos = xPosition;
+                yPos = yPosition;
+                isAlive = true;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override byte Collide(List<Image> playerShieldList, Image playerImage)
+        {
+            byte index = 0;
+
+            if (playerImage.Visibility == Windows.UI.Xaml.Visibility.Visible)
+            {
+                if (xPosition > Canvas.GetLeft(playerImage) - enemyBullet.Width && xPosition < Canvas.GetLeft(playerImage) + enemyBullet.Width)
+                {
+                    if (yPosition > Canvas.GetTop(playerImage) - enemyBullet.Height && yPosition < Canvas.GetTop(playerImage) + enemyBullet.Height)
+                    {
+                        return 4;
+                    }
+                }
+            }
+            index++;
+            
+            foreach(Image playerShield in playerShieldList)
+            {
+                if (playerShield.Visibility == Windows.UI.Xaml.Visibility.Visible)
+                {
+                    if (xPosition > Canvas.GetLeft(playerShield) - enemyBullet.Width && xPosition < Canvas.GetLeft(playerShield) + enemyBullet.Width)
+                    {
+                        if (yPosition > Canvas.GetTop(playerShield) - enemyBullet.Height && yPosition < Canvas.GetTop(playerShield) + enemyBullet.Height)
+                        {
+                            return index;
+                        }
+                    }
+                }
+            }                
+            return 255;
+        }
+
 
     }
 }
